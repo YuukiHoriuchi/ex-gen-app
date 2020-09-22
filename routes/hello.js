00fab7d8279774098723459072442dc78176caf4
YuukiHoriuchi <y.horiuchi@backtech.co.jp>
2020-09-22 13:44:53 +0900

    ユーザー追加機能の実装

--- routes/hello.js
+++ routes/hello.js
@@ -1,31 +1,65 @@
 const express = require('express');
 const router = express.Router();
 
-const sqlite3 = require('sqlite3');
+var mysql = require('mysql');
 
-// データベースオブジェクトの取得
-const db = new sqlite3.Database('mydb.sqlite3');
+
+
+// MySQLの設定情報
+var mysql_setting = {
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'mydb'
+};
+
 
 // GETアクセスの処理
 router.get('/',(req, res, next) => {
-  db.serialize(() => {
-    var rows = "";
-    db.each("select * from mydata",(err, row) => {
-      if (!err) {
-        rows += "<tr><th>" + row.id + "</th><td>"
-          + row.name + "</td><td></tr>";
-      }   
-    }, (err, count) => {
-      if (!err){
-        var data = {
-          title: 'hello!!',
-          content: rows
-        };
-        console.log(db);
-        res.render('hello', data);        
+  // コネクションの用意
+  var connection = mysql.createConnection(mysql_setting);
+  // データベースに接続
+  connection.connect();
+  // データを取り出す
+  connection.query('SELECT * from mydata', 
+          function (error, results, fields) {
+      // データベースアクセス完了時の処理
+      if (error == null) {
+          var data = {title:'mysql',content:results};
+          res.render('hello', data);
       }
-    }); 
   });
+  // 接続を解除
+  connection.end();
+});
+
+// 新規作成ページへのアクセス
+router.get('/add', (req, res, next) => {
+  var data = {
+      title: 'Hello/Add',
+      content: '新しいレコードを入力：'
+  }
+  res.render('hello/add', data);
+});
+
+
+// 新規作成フォーム送信の処理
+router.post('/add', (req, res, next) => {
+  var nm = req.body.name;
+  var ml = req.body.mail;
+  var ag = req.body.age;
+  var data = {'name':nm, 'mail':ml, 'age':ag};
+  // データベースの設定情報
+  var connection = mysql.createConnection(mysql_setting);
+  // データベースに接続
+  connection.connect();
+  // データを取り出す
+  connection.query('insert into mydata set ?', data, 
+          function (error, results, fields) {
+      res.redirect('/hello');
+  });
+  // 接続を解除
+  connection.end();
 });
 
 module.exports = router;
