61ae8d7131d80b9fb984d35a1cafe9946639561e
YuukiHoriuchi <y.horiuchi@backtech.co.jp>
2020-09-22 14:35:53 +0900

    修正機能、ユーザー削除機能の追加

--- routes/hello.js
+++ routes/hello.js
@@ -62,4 +62,115 @@ router.post('/add', (req, res, next) => {
   connection.end();
 });
 
+// 指定IDのレコードを表示する
+router.get('/show', (req, res, next) => {
+  var id = req.query.id;
+  // データベースの設定情報
+  var connection = mysql.createConnection(mysql_setting);
+  // データベースに接続
+  connection.connect();
+  // データを取り出す
+  connection.query('SELECT * from mydata where id=?', id,
+          function (error, results, fields) {
+      // データベースアクセス完了時の処理
+      if (error == null) {
+          var data = {
+              title: 'Hello/show',
+              content: 'id = ' + id + ' のレコード：',
+              mydata: results[0]
+          }
+          res.render('hello/show', data);
+      }
+  });
+  // 接続を解除
+  connection.end();
+});
+
+// 指定レコードを編集
+router.get('/edit', (req, res, next) => {
+  var id = req.query.id;
+  // データベースの設定情報
+  var connection = mysql.createConnection(mysql_setting);
+  // データベースに接続
+  connection.connect();
+  // データを取り出す
+  connection.query('SELECT * from mydata where id=?', id,
+          function (error, results, fields) {
+      // データベースアクセス完了時の処理
+      if (error == null) {
+          var data = {
+              title: 'Hello/edit',
+              content: 'id = ' + id + ' のレコード：',
+              mydata: results[0]
+          }
+          res.render('hello/edit', data);
+      }
+  });
+  // 接続を解除
+  connection.end();
+});
+
+
+// 編集フォーム送信の処理
+router.post('/edit', (req, res, next) => {
+  var id = req.body.id;
+  var nm = req.body.name;
+  var ml = req.body.mail;
+  var ag = req.body.age;
+  var data = {'name':nm, 'mail':ml, 'age':ag};
+  // データベースの設定情報
+  var connection = mysql.createConnection(mysql_setting);
+  // データベースに接続
+  connection.connect();
+  // データを取り出す
+  connection.query('update mydata set ? where id = ?', 
+          [data, id], function (error, results, fields) {
+      res.redirect('/hello');
+  });
+  // 接続を解除
+  connection.end();
+});
+
+// 指定レコードを削除
+router.get('/delete', (req, res, next) => {
+  var id = req.query.id;
+  // データベースの設定情報
+  var connection = mysql.createConnection(mysql_setting);
+  // データベースに接続
+  connection.connect();
+  // データを取り出す
+  connection.query('SELECT * from mydata where id=?', id,
+          function (error, results, fields) {
+      // データベースアクセス完了時の処理
+      if (error == null) {
+          var data = {
+              title: 'Hello/delete',
+              content: 'id = ' + id + ' のレコード：',
+              mydata: results[0]
+          }
+          res.render('hello/delete', data);
+      }
+  });
+  // 接続を解除
+  connection.end();
+});
+
+
+// 削除フォームの送信処理
+router.post('/delete', (req, res, next) => {
+  var id = req.body.id;
+  // データベースの設定情報
+  var connection = mysql.createConnection(mysql_setting);
+  // データベースに接続
+  connection.connect();
+  // データを取り出す
+  connection.query('delete from mydata where id=?', 
+          id, function (error, results, fields) {
+      res.redirect('/hello');
+  });
+  // 接続を解除
+  connection.end();
+});
+
+
 module.exports = router;
