5ea564ddc842b2cdc22cea64801d19a5b1411aa9
YuukiHoriuchi <y.horiuchi@backtech.co.jp>
2020-09-22 21:36:16 +0900

    delete機能付与

--- routes/users.js
+++ routes/users.js
@@ -33,5 +33,44 @@ router.post('/add',(req, res, next)=> {
     });
 });
 
+router.get('/edit',(req, res, next)=> {
+  db.users.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Edit',
+      form: usr
+    }
+    res.render('users/edit', data);
+  });
+});
+
+router.post('/edit',(req, res, next)=> {
+  db.users.findByPk(req.body.id)
+  .then(usr => {
+    usr.name = req.body.name;
+    usr.pass = req.body.pass;
+    usr.mail = req.body.mail;
+    usr.age = req.body.age;
+    usr.save().then(()=>res.redirect('/users'));
+  });
+});
+
+router.get('/delete',(req, res, next)=> {
+  db.users.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Delete',
+      form: usr
+    }
+    res.render('users/delete', data);
+  });
+});
+
+router.post('/delete',(req, res, next)=> {
+  db.users.findByPk(req.body.id)
+  .then(usr => {
+    usr.destroy().then(()=>res.redirect('/users'));
+  });
+});
 
 module.exports = router;
